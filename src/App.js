9a993ed199bc1041bef96e2d96e9f842d8938161
Ganto23 <godfrey.antomarlin@gmail.com>
2025-04-16 01:12:55 +0100

    Initial commit for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Header from './components/layout/Header';
 import Introduction from './components/layout/Introduction';
